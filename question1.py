4c3b1ac2e690ef6d3bbfe58b97300afb11c974ff
Irum Ghafoor <irumghafoor852@gmail.com>
2025-03-25 00:07:33 +0500

    Updated files

---
+++ question1.py
@@ -0,0 +1,46 @@
+#Question 1
+#Question: Write a program which will find all such numbers 
+#which are divisible by 7 but are not a multiple of 5, 
+#between 2000 and 3200 (both included). 
+#The numbers obtained should be printed in a comma-separated 
+#sequence on a single line.
+
+#create an empty list
+#num = []
+#for i in range(2000, 3201):
+#    if i%7 == 0 and i%5 != 0:
+#        num.append(str(i))
+
+#print(",".join(num))  
+
+  
+#converting int to string
+#type casting
+a = "10"
+b = str(10)
+print(a+b)
+
+#Define a function that can convert a integer into a string and print it in console.
+#Hints:
+#Use str() to convert a number to string.
+
+#creating a function
+#passing a parameter
+def int_to_string(number):
+    num1 = str(number)
+    print(num1)
+#calling back the function
+int_to_string(19999)
+
+
+
+
+         
+         
+         
+    
+
+         
+
+
+
